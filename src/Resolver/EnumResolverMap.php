531639cc5a4d4aaae304650d938e0a18cc087d83
Raoul Hofmann <raouldhofmann@gmail.com>
2022-05-25 23:20:25 +0800

    additional enum queries

--- src/Resolver/EnumResolverMap.php
+++ src/Resolver/EnumResolverMap.php
@@ -9,6 +9,7 @@ use Overblog\GraphQLBundle\Definition\Argument;
 use Overblog\GraphQLBundle\Resolver\ResolverMap;
 
 use App\Enum\Product;
+use App\Enum\RegionCode;
 
 class EnumResolverMap extends ResolverMap
 {
@@ -17,11 +18,27 @@ class EnumResolverMap extends ResolverMap
         return [
             "Enum" =>
             [
-                "product" => function ($value, Argument $args) {
+                "productByValue" => function ($value, Argument $args) {
                     return Product::tryFrom($args["value"]);
+                },
+                "productByName" => function ($value, Argument $args) {
+                    $products = Product::cases();
+                    $key = array_search($args["name"], array_column($products, 'name'));
+                    return $products[$key];
                 },
                 "products" => function ($value, Argument $args) {
                     return Product::cases();
+                },
+                "regionCodeByValue" => function ($value, Argument $args) {
+                    return RegionCode::tryFrom($args["value"]);
+                },
+                "regionCodeByName" => function ($value, Argument $args) {
+                    $codes = RegionCode::cases();
+                    $key = array_search($args["name"], array_column($codes, 'name'));
+                    return $codes[$key];
+                },
+                "regionCodes" => function ($value, Argument $args) {
+                    return RegionCode::cases();
                 }
             ]
         ];
